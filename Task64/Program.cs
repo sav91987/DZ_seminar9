cb260f35dc698afbdff9a03a70179ed762190380
Sotnikov_Andrey <sav91987@ya.ru>
2023-03-12 10:27:45 +0300

    added Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end)
+{
+    if (start == 1) return end.ToString();
+    return (start + "," + PrintNumbers(start - 1, end));
+}
+System.Console.WriteLine(PrintNumbers(N, 1));
